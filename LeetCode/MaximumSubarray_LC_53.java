c38203bc5c8d5f258ec950510c50ea06cd351ace
Ranju Kumari <rjsky009@gmail.com>
2024-11-30 06:31:39 +0530

    Create MaximumSubarray_LC_53.java

---
+++ LeetCode/MaximumSubarray_LC_53.java
@@ -0,0 +1,42 @@
+import static java.lang.Math.max;
+
+public class MaximumSubarraySum {
+
+    public static void main(String [] args){
+        int [] a = {-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println("Maximum sum is "+ maxSum_BigOofNsquareSoln(a));
+//        System.out.println("Maximum sum is "+ maxSum_KadanesAlgo(a));
+
+    }
+
+  //Brute force approach : O(n*n)
+    public static int maxSum_BigOofNsquareSoln(int [] a){
+
+        int maxSum = Integer.MIN_VALUE;
+        int n = a.length;
+        int sum = 0;
+        for(int i=0; i<=n-1;i++){
+            sum = 0;
+
+            for(int j=i; j<=n-1;j++){
+                sum+=a[j];
+                maxSum = max(sum, maxSum);
+            }
+        }
+        return maxSum;
+    }
+
+  //Kadane's algo : O(n)
+    public static long maxSum_KadanesAlgo(int [] a){
+
+        long sum=0;
+        long maxSum = -1;
+        int n=a.length;
+        for(int i =0;i<=n-1;i++){
+            sum+=a[i];
+            if(sum>maxSum){
+                maxSum = sum;
+            }
+            if(sum<0){
+                sum=0;
+            }
